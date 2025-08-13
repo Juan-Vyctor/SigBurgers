bb373b5ec6d1d56cba93147831aab1f7c9eb7302
Juan-Vyctor <juanvyctor.sgo@gmail.com>
2025-08-13 09:58:11 -0300

    template menus

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main(void) {
     printf("Bem vindo a Sig Burguers!");
+
+    int operacao_principal;
+    do {
+        scanf("%d", &operacao_principal);
+
+        if (operacao_principal == 1) {
+            printf("Aqui está o Cardápio");
+        } else if (operacao_principal == 2) {
+            printf("Faça o seu pedido");
+        } else if (operacao_principal == 3) {
+            printf("Aqui está o histórico dos seus pedidos");
+        }
+    } while (operacao_principal != 0);
     return 0;
 }
